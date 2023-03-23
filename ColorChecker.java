bb03daa0a3598ce761f5b9c6dee238eba11e4166
Edem <edemq123@gmail.com>
2023-03-23 20:35:15 +0200

    Update ColorChecker.java
    second check

--- ColorChecker.java
+++ ColorChecker.java
@@ -15,6 +15,7 @@ public class ColorChecker {
     private static EV3ColorSensor colorSensor = new EV3ColorSensor(SensorPort.S3);
    // private static EV3ColorSensor colorSensor = new EV3ColorSensor(SensorPort.S2);
     //checking if it updated
+    //second check to see if update to edem branch
     private static EV3UltrasonicSensor distanceChecker = new EV3UltrasonicSensor(SensorPort.S1);
 
     public static void main(String[] args){
