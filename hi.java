28c16114263b90f05ee246010c5b9b81cc7b8fb8
TeeniJai <112954731+TeeniJai@users.noreply.github.com>
2023-04-25 15:37:08 +0300

    Update hi.java
    Squash fact

--- hi.java
+++ hi.java
@@ -5,6 +5,8 @@ public class hi {
       System.out.println("Hi");
   
   //Hey, I hope this works! :3 //
+  //Did you know, squash is actually a gourd family? The more you know :D//
+  //Also, one of the gourds is called a "Pattypan"!//
   }
   
 }
