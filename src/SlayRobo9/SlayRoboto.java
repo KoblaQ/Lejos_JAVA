55ce0922a0efe246f95d49ce86198a54c89b1f17
Edem <edemq123@gmail.com>
2023-05-04 11:27:13 +0300

    Instructions added
    Additional guidlines added to the codes

--- src/SlayRobo9/SlayRoboto.java
+++ src/SlayRobo9/SlayRoboto.java
@@ -8,6 +8,7 @@ public class SlayRoboto {
 		private static LineFollower LFObj = new LineFollower(DE);
 		private static ObstacleDetector ODObj = new ObstacleDetector(DE);
 		private static ColorSensor CSObj = new ColorSensor(DE);
+//		START THE HTTPTEST OBJECT HERE
 
 		public static void main(String[] args) {
 			// TODO Auto-generated method stub
@@ -15,7 +16,7 @@ public class SlayRoboto {
 			ODObj.start();
 			LFObj.start();
 			CSObj.start();
-			
+//			START IT HERE TOO JUST LIKE ABOVE
 			
 			while(!(Button.getButtons()!=0)) {
 				//Break if button is pressed
